cbe5268c06293f774e36b837e2598428ed0fff00
osmanrahimi <mr.osmanrahimi@gmail.com>
2017-11-27 19:52:49 +0330

    d

---
+++ Migrations/20171127145853_SeedDatabase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                    migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES('MAKE1');");
+                    migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES('MAKE2');");
+                    migrationBuilder.Sql("INSERT INTO MAKES (NAME) VALUES('MAKE3');");
+
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE1-MODEL1',(select Id from makes where name='MAKE1'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE1-MODEL2',(select Id from makes where name='MAKE1'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE1-MODEL3',(select Id from makes where name='MAKE1'))");
+
+
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE2-MODEL1',(select Id from makes where name='MAKE2'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE2-MODEL2',(select Id from makes where name='MAKE2'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE2-MODEL3',(select Id from makes where name='MAKE2'))");
+
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE3-MODEL1',(select Id from makes where name='MAKE3'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE3-MODEL2',(select Id from makes where name='MAKE3'))");
+                    migrationBuilder.Sql("INSERT INTO MODELS (NAME,MAKEID) VALUES('MAKE3-MODEL3',(select Id from makes where name='MAKE3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM MAKES");
+        }
+    }
+}
