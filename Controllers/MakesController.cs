7305bddd85500abd9bb09efe6ad2b0bf3651a72f
osmanrahimi <mr.osmanrahimi@gmail.com>
2017-11-28 16:22:43 +0330

    Add Make Controller

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using vega.Models;
+namespace Vega.Controllers
+{
+
+    public class MakesController: Controller
+    {
+        public MakesController()
+        {
+        }
+
+       
+    }
+}
