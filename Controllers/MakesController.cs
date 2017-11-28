96972860764e1f631754dc2f94091b50a48fc711
osmanrahimi <mr.osmanrahimi@gmail.com>
2017-11-28 16:59:19 +0330

    a

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -1,16 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using vega.Models;
+using System.Collections.Generic;
+using vega.Persistence;
+
 namespace Vega.Controllers
 {
 
     public class MakesController: Controller
     {
-
-        
-        public MakesController()
-        {
-
-        }
 
        
     }
